ab858ab87661a69367b8cfd35563e0a854e8107c
Urtellik <205469920+urtellik@users.noreply.github.com>
2025-04-07 17:58:55 -0400

    Log current version

--- src/scripts/urtellikUL.lua
+++ src/scripts/urtellikUL.lua
@@ -1,1 +1,8 @@
 urtellikUL = urtellikUL or {}
+
+local loginator = require("urtellikUL.mdk.loginator")
+local log = loginator:new({
+  name = "urtellikUL.dataCapture.dataTag",
+  level = "info"
+})
+log:info("UULC version @VERSION@")
