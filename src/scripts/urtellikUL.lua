8ba9385fb25fa0625e0426d03561e640727d24e9
Urtellik <205469920+urtellik@users.noreply.github.com>
2025-04-07 17:59:41 -0400

    Fix logger name

--- src/scripts/urtellikUL.lua
+++ src/scripts/urtellikUL.lua
@@ -2,7 +2,7 @@ urtellikUL = urtellikUL or {}
 
 local loginator = require("urtellikUL.mdk.loginator")
 local log = loginator:new({
-  name = "urtellikUL.dataCapture.dataTag",
+  name = "urtellikUL",
   level = "info"
 })
 log:info("UULC version @VERSION@")
